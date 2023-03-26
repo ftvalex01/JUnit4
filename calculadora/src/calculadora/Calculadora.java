e3b1a4e45aa0c8f34d7ad6aa04834ef2c06fc8b7
ftvalex01 <ftvalex@hotmail.com>
2023-03-26 15:26:59 +0100

    Ejercicio2

--- calculadora/src/calculadora/Calculadora.java
+++ calculadora/src/calculadora/Calculadora.java
@@ -1,6 +1,7 @@
 package calculadora;
 
 public class Calculadora {
+	
 	private int num1;
 	private int num2;
 	
@@ -13,14 +14,32 @@ public class Calculadora {
 		return result;
 	}
 	public int resta() {
-		int result = num1 - num2;
+		int result;
+		if(resta2()) {
+			result = num1 - num2;
+		}else {
+			result = num2- num1;
+		}
 		return result;
+	}
+	public boolean resta2() {
+		if(num1 >= num2) {
+			return true;
+			
+		}else {
+			return false;
+		}
 	}
 	public int multiplicacion() {
 		int result = num1 * num2;
 		return result;
 	}
 	public int division() {
+		int result = num1 / num2;
+		return result;
+	}
+	public Integer divide2() {
+		if(num2 == 0) return null;
 		int result = num1 / num2;
 		return result;
 	}
